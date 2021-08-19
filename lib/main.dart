9756a3901681de5e9f47f8c7c0d46d3aa5446cc9
Andy <andyzhao0808@gmail.com>
2021-08-20 02:26:47 +0700

    getX简单应用；

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(GetMaterialApp(home: Home()));
+
+
+///controller管理State、处理逻辑业务
+class Controller extends GetxController {
+  var count = 0.obs;
+  
+  increment() => count++;
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+///View包含controller实例，通过实例获取状态，更新状态
+class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  Widget build(context) {
+    // 使用Get.put()实例化你的类，使其对当下的所有子路由可用。
+    final Controller control = Get.put(Controller());
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        // 使用Obx(()=>每当改变计数时，就更新Text()。
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${control.count}"))),
+
+        // 用一个简单的Get.to()即可代替Navigator.push那8行，无需上下文！
+        body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    child: Text("Go to Other"), onPressed: () => Get.to(Other())),
+                ///展示snackBar
+                ElevatedButton(
+                    child: Text("show snackBar"), onPressed: () => Get.snackbar('title', 'message')),
+                ///展示dialog
+                ElevatedButton(
+                    child: Text("show dialog"), onPressed: () => Get.defaultDialog(title: 'what are you doing')),
+                ///改变主题颜色
+                ElevatedButton(
+                    child: Text("change ThemeData"), onPressed: () => Get.changeTheme(ThemeData.dark())),
+                ///改变主题颜色
+                ElevatedButton(
+                    child: Text("change ThemeData"), onPressed: () => Get.changeTheme(ThemeData.light())),
+              ],
+            )),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: control.increment));
+  }
+}
+
+class Other extends StatelessWidget {
+  // 你可以让Get找到一个正在被其他页面使用的Controller，并将它返回给你。
+  final Controller c = Get.find();
+
+  @override
+  Widget build(context) {
+    // 访问更新后的计数变量
+    return Scaffold(body: Center(child: Text("${c.count}")));
   }
 }
